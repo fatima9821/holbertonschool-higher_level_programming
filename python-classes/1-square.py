a27b3a6190cc503b5a3c8685841e8d3538f8b78a
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-07 11:52:19 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines an empty square class.
+"""
+
+
 class Square:
+
+    """
+    This class represents a square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes an instance of the square class.
+        """
         self.__size = size
         pass
