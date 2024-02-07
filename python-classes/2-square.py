f71f6ae293d0be880210569e4affb4d3fa0a38f3
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-07 12:46:54 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
+"""
+This module define an empty square
+"""
+
+
 class Square:
+    """
+    This class represents a square.
+    """
     def __init__(self, size=0):
+
+        """
+        Check if the size is an integer
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        """
+        check if size is egale to zero
+        """
+
         if size < 0:
             raise TypeError("size must be >= 0")
+        """
+        attribute private for stock a size of square
+        """
+
         self.__size = size
