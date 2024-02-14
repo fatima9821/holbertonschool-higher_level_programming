77357d7f7fc16e2c81c86e90d3dfbaf217b30fa0
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-14 03:50:54 -0800

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that return true if the object otherwise false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ kind of class """
+    return isinstance(obj, a_class)
