efacbd8d3676e5d8a85986a86e32598ea7f4f636
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-15 13:02:58 -0800

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+function that writes a string to a text
+"""
+
+
+def write_file(filename="", text=""):
+    """write a string"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
