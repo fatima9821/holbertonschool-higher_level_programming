b35a0f286ec4323e7205c7100c8cde5074512b54
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-22 07:06:27 -0800

    square.py

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module inherits from rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class represent the square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        initialize an square with size specifice
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ return a string representation of the square """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
