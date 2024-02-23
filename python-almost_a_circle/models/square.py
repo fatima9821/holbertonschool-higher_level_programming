5defe52449ef71c08d3c4a5330ee49494fdb2846
fatima9821 <bahfatoumat9821@gmail.com>
2024-02-23 12:20:02 -0800

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python3
-"""
-module inherits from rectangle
-"""
+"""The class of square"""
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class represent the square """
+    """declaration of the class squre inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        initialize an square with size specifice
-        """
+        """inistalizing method of the class square constructor"""
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """update attribute if exist"""
+        if args:
+            attributes = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
-        """ return a string representation of the square """
+        """Return the string that represente a square"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    def to_dictionary(self):
+        """the dictionary"""
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
