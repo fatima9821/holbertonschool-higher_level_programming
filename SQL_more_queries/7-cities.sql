039b7ff628d9974d2842f3dd847fbc8d1c1230f4
root <root@FAT>
2024-03-20 13:42:45 +0100

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 -- script that create the table
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT  UNIQUE NOT NULL PRIMARY KEY,
-	states_id INT NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
