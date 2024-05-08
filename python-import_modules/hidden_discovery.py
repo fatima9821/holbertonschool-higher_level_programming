826608c037b3c8f64f959ccd220178250bec6823
fatima9821 <146451776+fatima9821@users.noreply.github.com>
2024-05-08 19:12:12 +0200

    Delete python-import_modules/hidden_discovery.py

--- python-import_modules/hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from hidden_4 import *
-    names = dir()
-    for i in range(0, len(names)):
-        if (names[i][:2] != "__"):
-            print("{:s}".format(names[i]))
