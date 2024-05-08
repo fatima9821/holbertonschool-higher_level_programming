ad03c33693093d540a759c2327a20c86bfe58ce4
root <root@FAT>
2024-05-08 19:19:09 +0200

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    from hidden_4 import *
+    names = dir()
+    for i in range(0, len(names)):
+        if (names[i][:2] != "__"):
+            print("{:s}".format(names[i]))
