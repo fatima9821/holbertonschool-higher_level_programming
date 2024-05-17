78b18b0d29ba194b5b760f484a397e976582bb1f
root <root@FAT>
2024-05-17 22:04:26 +0200

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+    if not a_dictionary or a_dictionary is None:
+        return (None)
 
+    best_value = float('-inf')
+    best_key = None
 
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_value = value
+            best_key = key
+
+    return (best_key)
