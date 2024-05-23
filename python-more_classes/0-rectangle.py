385b47d4d6792ddcc2db1d05ae247a398d8ce09f
root <root@FAT>
2024-05-23 15:02:03 +0200

    mon projet

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines an empty class rectangle.
+
+The Square class represents a geometric rectangle.
+"""
+
+
+class Rectangle:
+
+    """This class defines a rectangle.
+
+    Attributes:
+        None.
+
+    Methods:
+        None.
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+phython more class
