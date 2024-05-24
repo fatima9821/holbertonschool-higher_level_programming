1d3a1f006365860ce390340fb7674e9c51137156
root <root@FAT>
2024-05-24 21:21:13 +0200

    mon p

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+# Define the abstract base class
+class Animal(ABC):
+    
+    @abstractmethod
+    def sound(self):
+        pass
+
+# Define the Dog class inheriting from Animal
+class Dog(Animal):
+    
+    def sound(self):
+        return "Bark"
+
+# Define the Cat class inheriting from Animal
+class Cat(Animal):
+    
+    def sound(self):
+        return "Meow"
