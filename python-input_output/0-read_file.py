bef41f5ce640cbe1c9864ac4009ab59478f78e38
root <root@FAT>
2024-05-27 10:34:54 +0200

    mon p

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+le script est une fonction qui lit le contenu
+dun fichier texte(UTF8) dans la sortie stantard
+"""
+
+
+def read_file(filename=""):
+    """lire le contenu du fichier et l'afficher
+    Args:
+    filename: le chemin vers le fichier a lire
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        texte = file.read()
+        print(texte)
