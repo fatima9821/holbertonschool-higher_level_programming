c35eef038e642cc2ef4fe9d72db2b95378be30ef
root <root@FAT>
2024-05-28 14:55:02 +0200

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script qui ajoute tous les
+arguments à une liste Python,
+puis enregistrez-les dans un fichier
+"""
+from sys import argv
+
+
+save = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
+filename = "add_item.json"
+try:
+    list = load(filename)
+except Exception:
+    list = []
+for argumments in argv[1:]:
+    list.append(argumments)
+save(list, filename)i
