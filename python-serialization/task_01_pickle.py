c94a2aeb84a88fd34d0bca16f6122a625c8f80d4
root <root@FAT>
2024-05-29 16:35:36 +0200

    task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+serelise et deserelise
+un obj python a laide de pickle
+"""
+import json
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Erreur lors de la sérialisation : {e}")
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except (FileNotFoundError, pickle.UnpicklingError, Exception) as e:
+            print(f"Erreur lors de la désérialisation : {e}")
+            return None
