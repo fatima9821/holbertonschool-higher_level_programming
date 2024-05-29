711f0a7c348d451ae713591a6f8e60b7f1b60edd
root <root@FAT>
2024-05-29 16:03:53 +0200

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,14 +3,13 @@
 creation de module de sterelisation
 dun dic dans un json
 """
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, "w", encoding="utf-8") as outfile:
         json.dump(data, outfile)
-    pass
 
 def load_and_deserialize(filename):
-    with open(filename, "w", encoding="utf-8") as infile:
+    with open(filename, "r", encoding="utf-8") as infile:
         return json.load(infile)
-    pass
