f2c85cffbf1058e3813ed41ecef07b5d4e7d5e23
root <root@FAT>
2024-06-23 20:21:48 +0200

    fixing errors

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,8 +16,11 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
