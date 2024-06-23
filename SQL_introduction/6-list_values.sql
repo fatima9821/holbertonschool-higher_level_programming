509ce9a549261c0f116b6d4a0c8576b5bb1cef3b
root <root@FAT>
2024-06-23 14:57:37 +0200

    mon

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- script that lists alls rows of the table
+SELECT * FROM first_table;
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- script that creates a table in the database
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	scrore INT
+);
+INERT INTO second_table (id, name, score) VALUES (1, 'John', 10)
+INERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3)
+INERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14)
+INERT INTO second_table (id, name, score) VALUES (4, 'George', 8)
